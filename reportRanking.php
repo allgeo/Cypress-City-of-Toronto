fc7a1c1b495f0a37ec4c44836a6f0c7e380078ca
nikkikapadia <72356613+nikkikapadia@users.noreply.github.com>
2021-04-08 16:28:51 -0400

    Update reportRanking.php

--- reportRanking.php
+++ reportRanking.php
@@ -38,7 +38,7 @@ if ($conn->connect_error) {
 
         <div class="register-content">
           <a href="reportsHome.html" style="float: left; margin: 4%">BACK</a>
-          <a href="index.html" style="float: right; margin: 4%">LOGOUT</a>
+          <a href="logout.php" style="float: right; margin: 4%">LOGOUT</a>
           <br> <br> <br>
 
           <h1>Reports Ranked from Highest Imporance to Lowest</h1>
