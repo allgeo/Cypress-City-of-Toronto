7636c1551023465745aa16eeb552739cc8200304
nikkikapadia <72356613+nikkikapadia@users.noreply.github.com>
2021-04-08 16:42:02 -0400

    Update suggest.php

--- suggest.php
+++ suggest.php
@@ -39,7 +39,7 @@ if ($conn->connect_error) {
 
         <div class="register-content">
           <a href="portal.html" style="float: left; margin: 4%">BACK</a>
-          <a href="index.html" style="float: right; margin: 4%">LOGOUT</a>
+          <a href="logout.php" style="float: right; margin: 4%">LOGOUT</a>
           <br> <br> <br>
 
           <?php
