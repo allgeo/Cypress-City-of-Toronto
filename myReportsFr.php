0b72b88034cd93ace4715e227a821cd428c3d289
nikkikapadia <72356613+nikkikapadia@users.noreply.github.com>
2021-04-09 11:31:54 -0400

    Update myReportsFr.php

--- myReportsFr.php
+++ myReportsFr.php
@@ -23,7 +23,39 @@ if ($conn->connect_error) {
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@1,600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"/>
+    <script src="https://api.tiles.mapbox.com/mapbox-gl-js/v2.2.0/mapbox-gl.js"></script>
+    <link href="https://api.tiles.mapbox.com/mapbox-gl-js/v2.2.0/mapbox-gl.css" rel="stylesheet" />
     <title>Mes Rapports</title>
+    <style>
+        body {
+        margin: 0;
+        padding: 0;
+        }
+        #map {
+        position: relative;
+        margin-left: 60px;
+        margin-top: 15px;
+        margin-bottom: 50px;
+        width: 75%;
+        height: 300px;
+        }
+        .marker {
+        background-image: url('mapbox-icon.png');
+        background-size: cover;
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+        cursor: pointer;
+        }
+        .mapboxgl-popup {
+        max-width: 200px;
+        }
+        .mapboxgl-popup-content {
+        text-align: center;
+        font-family: 'Open Sans', sans-serif;
+        }
+    </style>
   </head>
   <body>
     <div class="inner-reg">
@@ -40,7 +72,47 @@ if ($conn->connect_error) {
         <div class="register-content">
           <a href="reportsHomeFr.html" style="float: left; margin: 4%">Retournez</a>
           <a href="logoutFr.php" style="float: right; margin: 4%">Se Déconnecter</a>
-          <br> <br>
+          <br>
+            
+           <div class="inner-reg-2">
+                <h2 style="margin-left: 1%"> Le plus de rapports: le centre-ville de Toronto </h2>  
+                <div id="map">cszczcxz</div>
+                <script>
+                  mapboxgl.accessToken = 'pk.eyJ1IjoidHVyamEyMCIsImEiOiJja240cm9jMnIwMGdhMnZsOG5ta3JyN29kIn0.eHftLsKhf3GNOAoZY-4SbQ';
+
+
+
+                  var map = new mapboxgl.Map({
+                  container: 'map',
+                  style: 'mapbox://styles/turja20/ckn4t27pa21iq17mv2o520vfc',
+                  center: [-79.3575411167036,43.65649083874243],
+                  zoom: 11
+                  });
+
+                  // add markers to map
+                  geojson.features.forEach(function (marker) {
+                  // create a HTML element for each feature
+                  var el = document.createElement('div');
+                  el.className = 'marker';
+
+                  // make a marker for each feature and add it to the map
+                  new mapboxgl.Marker(el)
+                  .setLngLat(marker.geometry.coordinates)
+                  .setPopup(
+                  new mapboxgl.Popup({ offset: 25 }) // add popups
+                  .setHTML(
+                  '<h3>' +
+                  marker.properties.title +
+                  '</h3><p>' +
+                  marker.properties.description +
+                  '</p>'
+                  )
+                  )
+                  .addTo(map);
+                  });
+                </script>
+                </div> 
+            
           <?php
           $command = "";
           if (isset($_SESSION['user_id'])) {
